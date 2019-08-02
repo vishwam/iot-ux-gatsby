910fe4450c99d38d222bf46ea1364d17514803fc
Vishwam Subramanyam <v@4261.in>
2019-08-02 07:23:03 -0700

    Support @microsoft/azure-iot-ux-fluent-controls with theming

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,92 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path');
+const PostCssNormalize = require('postcss-normalize');
+
+const root = process.cwd();
+const themePaths = [
+    // for some reason, we need to add another path three folders deep in order to compile
+    // our styles correctly. @todo figure out why and remove this
+    path.resolve(root, './foo/bar/baz'),
+
+    // make src/theme/_colors.scss importable globally. This is required for fluent-controls,
+    // which does an "@import colors": we want this to resolve to ux-renderer's colors, which
+    // imports fluent-css's colors and applies overrides on top of it:
+    path.resolve(root, './src/theme/'),
+];
+
+exports.onCreateWebpackConfig = ({
+    stage,
+    rules,
+    loaders,
+    plugins,
+    actions,
+}) => {
+    const isEnvProduction = stage !== 'develop' && stage !== 'develop-html';
+    const isSSR = stage.includes('html');
+    function getStyleLoaders(isModule) {
+        return [
+            !isSSR && loaders.miniCssExtract({ hmr: false }),
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: isModule,
+                    importLoaders: 2,
+                    localIdentName: isEnvProduction
+                        ? '[contenthash:8]'
+                        : '[local]_[contenthash:4]',
+                    sourceMap: true,
+                },
+            },
+            {
+                loader: 'postcss-loader',
+                options: {
+                    // Necessary for external CSS imports to work
+                    // https://github.com/facebookincubator/create-react-app/issues/2677
+                    ident: 'postcss',
+                    plugins: () => [
+                        require('postcss-preset-env')({
+                            autoprefixer: {
+                                flexbox: 'no-2009',
+                            },
+                            stage: 3,
+                        }),
+                        // Adds PostCSS Normalize as the reset css with default options,
+                        // so that it honors browserslist config in package.json
+                        // which in turn let's users customize the target behavior as per their needs.
+                        PostCssNormalize(),
+                    ],
+                    sourceMap: true,
+                },
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    includePaths: themePaths,
+                    sourceMap: true,
+                },
+            },
+        ].filter(Boolean);
+    }
+
+    actions.setWebpackConfig({
+        module: {
+            rules: [
+                {
+                    oneOf: [
+                        {
+                            test: /\.module\.(scss|sass)$/,
+                            use: getStyleLoaders(true),
+                        },
+                        {
+                            test: /.(scss|sass)$/,
+                            use: getStyleLoaders(false),
+                            sideEffects: true,
+                        },
+                    ]
+                }
+            ],
+        },
+    })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import { Shell, DateTimeField } from "@microsoft/azure-iot-ux-fluent-controls";
+
+import './shell.fonts.scss';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  return (
+    <Shell masthead={{
+      branding: 'My Gatsby Site'
+    }}>
+      <Layout>
+        <SEO title="Home" />
+        <h1>Hello World!</h1>
+        <div>{data.site.siteMetadata.description}</div>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <DateTimeField
+              name='date-picker-0'
+              initialValue={'Sep 20, 2010 05:00:00 GMT'}
+              label='Default example (Local)'
+              localTimezone={true}
+              onChange={(newValue) => alert(newValue)}
+        />
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    </Shell>
+  );
+}
+
+export const query = graphql`
+  query HomePageQuery {
+    site {
+      siteMetadata {
+        description
+      }
+    }
+  }
+`;
 
 export default IndexPage
